e82a455f66591c5c1506d594148927bc8f7e9d2f
Nolan Reilly <nolanreilly02@gmail.com>
2025-08-09 10:45:03 -0500

    Allow Users to Create Posts

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,4 +1,48 @@
 class PostsController < ApplicationController
+  before_action :authenticate_user!, only: [ :new, :create, :edit, :update, :destroy ]
+  before_action :authorize_user, only: [ :new, :create, :edit, :update, :destroy ]
+
   def index
+    @posts = Post.all
+  end
+
+  # Maybe this isn't the right spot because of our routes???
+
+  def show
+    @post = Post.find(params[:id])
+  end
+
+  def new
+    @post = current_user.posts.build
+  end
+
+  def create
+    @post = current_user.posts.build(post_params)
+
+    if @post.save
+     redirect_to @post, notice: "Post Successfully Created"
+    else
+      render :new, status: :unprocessable_entity
+    end
+  end
+
+  def edit
+  end
+
+  def update
+  end
+
+  def destroy
+  end
+
+  private
+
+  def authorize_user
+    @user = User.find(params[:user_id])
+    redirect_to root_path, alert: "Not authorized" unless @user == current_user
+  end
+
+  def post_params
+    params.require(:post).permit(:title, :content)
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,17 @@
 Rails.application.routes.draw do
   devise_for :users
+
   resources :users, only: [ :index, :show, :edit, :update, :destroy ] do
+    resources :posts, only: [ :new, :create, :edit, :update, :destroy ]
+
     member do
       post "follow"
       post "unfollow"
     end
   end
+
+  resources :posts, only: [ :index, :show ]
+
   get "up" => "rails/health#show", as: :rails_health_check
 
   root "posts#index"
